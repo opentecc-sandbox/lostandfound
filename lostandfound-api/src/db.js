c8fe8ccc23940c67fa0337e41519829e8ba8d752
hajar <hajarchoukaih@gmail.com>
2026-03-11 14:51:32 +0000

    US1: Setup backend with Prisma and Neon databasse

--- lostandfound-api/src/db.js
+++ lostandfound-api/src/db.js
@@ -2,11 +2,9 @@ const { Pool } = require("pg");
 require("dotenv").config();
 //pool is like a manager: whenever your backend needs to talk to the database, it asks the pool to get a connection.
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env .DB_URL,
+  ssl:{ rejectUnauthorized: false },
+ 
 });
 
 module.exports = pool;
